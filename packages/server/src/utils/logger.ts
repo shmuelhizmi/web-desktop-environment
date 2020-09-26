80c8cfd02ee71c947b2aed635651c5c19f047498
shmuelhizmi <shmuelhizmi1@gmail.com>
2020-09-26 13:03:37 +0300

    ⏰ add time indication to server logger

--- packages/server/src/utils/logger.ts
+++ packages/server/src/utils/logger.ts
@@ -1,5 +1,7 @@
 import color from "chalk";
 
+let globalLastLogDate = Date.now();
+
 export default class Logger {
 	level: string;
 	constructor(level?: string) {
@@ -9,6 +11,15 @@ export default class Logger {
 			this.level = color.bgBlack("root");
 		}
 	}
+	private getCurrentLogDelay = () => {
+		const now = Date.now();
+		const currentLogDelay = now - globalLastLogDate;
+		globalLastLogDate = now;
+		if (currentLogDelay > 1000) {
+			return currentLogDelay / 1000 + "sec";
+		}
+		return currentLogDelay + "ms";
+	};
 	private formatMessage = (message: string) => {
 		if (message.includes("\n")) {
 			return `
@@ -27,29 +38,37 @@ export default class Logger {
 
 	public info = (message: string) => {
 		console.info(
-			`[ ${this.level} ]: ${color.green(this.formatMessage(message))}`
+			`[ ${this.level} ]: ${color.green(
+				this.formatMessage(message)
+			)} ${this.getCurrentLogDelay()}`
 		);
 	};
 	public error = (message: string) => {
 		console.error(
-			`[ ${this.level} ]: ${color.red(this.formatMessage(message))}`
+			`[ ${this.level} ]: ${color.red(
+				this.formatMessage(message)
+			)} ${this.getCurrentLogDelay()}`
 		);
 	};
 	public warn = (message: string) => {
 		console.warn(
-			`[ ${this.level} ]: ${color.yellow(this.formatMessage(message))}`
+			`[ ${this.level} ]: ${color.yellow(
+				this.formatMessage(message)
+			)} ${this.getCurrentLogDelay()}`
 		);
 	};
 
 	public mount = (levelName: string) => {
-		return new Logger(`${this.level}:${this.randomBG()(levelName)}`);
+		return new Logger(`${this.level}:${this.randomBG(levelName)}`);
 	};
 
-	private randomBG = () => {
+	private randomBG = (text: string) => {
 		const randomColor = () => {
-			const r = Math.random() * 125; //not 255 to avoid white on white
+			const r = Math.random() * 125 + 125; //not 255 to avoid black on black
 			return r - (r % 1);
 		};
-		return color.bgRgb(randomColor(), randomColor(), randomColor());
+		return color.black(
+			color.bgRgb(randomColor(), randomColor(), randomColor())(text)
+		);
 	};
 }

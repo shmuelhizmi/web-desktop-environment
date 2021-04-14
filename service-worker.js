0f821ff00c7241e49a858ee2692051a9870c36ff
shmuelhizmi <shmuelhizmi1@gmail.com>
2021-04-14 17:41:04 +0300

    Updates

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/web-desktop-environment/precache-manifest.5ea8d38bec5ae6eaaba9d2cbd6fe879e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web-desktop-environment/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
---
+++ serviceworker.js
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
+
+const CACHE_NAME = "version=1";
+const urlsToCache = ["index.html"];
+
+// Install SW
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+// Listen for requests
+self.addEventListener("fetch", (event) => {
+	event.respondWith(
+		caches.open(CACHE_NAME).then((cache) => {
+			return cache.match(event.request).then((response) => {
+				return fetch(event.request)
+					.then((response) => {
+						if (
+							event.request.url.includes(self.location.host) &&
+							(event.request.destination === "script" ||
+								event.request.destination === "document" ||
+								event.request.url.endsWith(".html") ||
+								event.request.url.endsWith(".css") ||
+								event.request.url.endsWith(".js") ||
+								event.request.url.endsWith(".json"))
+						) {
+							cache.put(event.request, response.clone());
+						}
+						return response;
+					})
+					.catch(() => response);
+			});
+		})
+	);
+});
+
+// Activate the SW
+self.addEventListener("activate", (event) => {
+	const cacheWhitelist = [];
+	cacheWhitelist.push(CACHE_NAME);
+
+	event.waitUntil(
+		caches.keys().then((cacheNames) =>
+			Promise.all(
+				cacheNames.map((cacheName) => {
+					if (!cacheWhitelist.includes(cacheName)) {
+						return caches.delete(cacheName);
+					}
+				})
+			)
+		)
+	);
+});

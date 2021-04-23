e0b1de3b1a13cb85f2c1ebae2b1376bf05db5812
shmuelhizmi <shmuelhizmi1@gmail.com>
2021-04-23 08:24:38 +0300

    🌠 new feature - keep loggers print color between different runs

--- packages/sdk/server/src/utils/logger.ts
+++ packages/sdk/server/src/utils/logger.ts
@@ -59,16 +59,46 @@ export default class Logger {
 	};
 
 	public mount = (levelName: string) => {
-		return new Logger(`${this.level}:${this.randomBG(levelName)}`);
+		return new Logger(`${this.level}:${this.calculateBG(levelName)}`);
 	};
 
-	private randomBG = (text: string) => {
-		const randomColor = () => {
-			const r = Math.random() * 125 + 125; //not 255 to avoid black on black
-			return r - (r % 1);
-		};
-		return color.black(
-			color.bgRgb(randomColor(), randomColor(), randomColor())(text)
-		);
+	private calculateBG = (text: string) => {
+		const hash = `#${hashFnv32a(text, true, 19)}}`;
+		return color.black(color.bgHex(hash)(text));
 	};
+}
+
+/**
+ * Calculate a 32 bit FNV-1a hash
+ * Found here: https://gist.github.com/vaiorabbit/5657561
+ * Ref.: http://isthe.com/chongo/tech/comp/fnv/
+ *
+ * @param {string} str the input value
+ * @param {boolean} [asString=false] set to true to return the hash value as
+ *     6-digit hex string instead of an integer
+ * @param {integer} [seed] optionally pass the hash of the previous chunk
+ * @returns {integer | string}
+ */
+function hashFnv32a<AsString extends boolean>(
+	str: string,
+	asString: AsString,
+	seed: number
+): AsString extends true ? string : number {
+	/*jshint bitwise:false */
+	let i,
+		l,
+		hval = seed === undefined ? 0x811c9dc5 : seed;
+
+	for (i = 0, l = str.length; i < l; i++) {
+		hval ^= str.charCodeAt(i);
+		hval +=
+			(hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+	}
+	if (asString) {
+		// Convert to 8 digit hex string
+		return ("0000000" + (hval >>> 0).toString(16)).substr(
+			-6
+		) as AsString extends true ? string : number;
+	}
+	return (hval >>> 0) as AsString extends true ? string : number;
 }

4b5531376aad9e344f3ed68efe683a3022050202
Shmuel Hizmi <48323313+shmuelhizmi@users.noreply.github.com>
2022-05-12 19:59:08 +0000

    transpile only by default

--- packages/servers/development-edition-server/bin/index.js
+++ packages/servers/development-edition-server/bin/index.js
@@ -5,6 +5,7 @@ const { join } = require("path");
 
 require("ts-node").register({
 	project: join(__dirname, "../tsconfig.json"),
+	transpileOnly: !(process.env.DEBUG || "").includes("TRANSPILE"),
 });
 
 try {
